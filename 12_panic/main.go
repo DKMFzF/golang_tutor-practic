16c9f4c5d36fc2cbf2b60b41ca7a5280122ea5e4
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-14 20:42:12 +0700

    update 12_panic

--- 12_panic/main.go
+++ 12_panic/main.go
@@ -1,18 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Паника в Golang - это ошибка в runtime (любые ошибки при выполнении, которые нельзя обработать)
 
 func main() {
 
-	defer fmt.Println(printMessage())
-	defer fmt.Println("Хуй")
-	fmt.Println("main()")
+	defer handlerPanic()
 
-	panic("ААААА ПАНИКА!!")
+	messages := []string{
+		"msg1",
+		"msg2",
+		"msg3",
+		"msg4",
+		"msg5",
+	}
+
+	messages[5] = "msg6"
+
+	printMessage(messages)
 }
 
-func printMessage() string {
-	return "printMessage()"
+func printMessage(message []string) {
+	fmt.Println(message)
+}
+
+// Функция которая обрабатывает панику
+func handlerPanic() {
+	if r := recover(); r != nil {
+		fmt.Println(r) // выводит сообщение об ошибке
+	}
+	fmt.Println("handlerPanic() Выполнилась успешно")
 }
