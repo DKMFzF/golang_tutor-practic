8ecf01b83337b97557d5b22dd5036b5a5702e843
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-19 22:37:37 +0700

    add 22_bufer-canal

---
+++ 22_bufer-canal/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	// msg := make(chan string, 3)
+
+	// без буфуризации
+	// go func() {
+	// 	msg <- "Hello"
+	// 	msg <- "Привет мир!"
+	// 	msg <- "Привет мир! 2"
+	// }()
+
+	// // каждая операция с каналом блокируется выполнение go-рутины
+	// fmt.Println(<-msg)
+	// fmt.Println(<-msg)
+	// fmt.Println(<-msg)
+
+	// c буферизацией
+	// msg <- "Hello"
+	// msg <- "Привет мир!"
+	// msg <- "Привет мир! 2"
+
+	// fmt.Println(<-msg)
+
+	msg2 := make(chan string, 3)
+	msg2 <- "Привет 1"
+	msg2 <- "Привет 2"
+	msg2 <- "Привет 3"
+
+	close(msg2)
+
+	for {
+		if value, ok := <-msg2; !ok {
+			fmt.Println("chanel closed")
+			break
+		} else {
+			fmt.Println(value)
+		}
+	}
+}
