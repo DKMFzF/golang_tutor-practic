d80ab3005950f365dc16b7df8fe6ec340d6ea232
DKMFzF <flim.win@yandex.ru>
2025-11-15 23:44:06 +0700

    feat: update http

--- 27_http/cmd/modules/main.go
+++ 27_http/cmd/modules/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +10,7 @@ import (
 
 func mainPage(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("Привет!"))
+	res.WriteHeader(http.StatusAccepted)
 }
 
 func apiRoute(res http.ResponseWriter, req *http.Request) {
@@ -32,6 +34,7 @@ func apiRoute(res http.ResponseWriter, req *http.Request) {
 		body += fmt.Sprintf("%s: %v\r\n", k, v)
 	}
 
+	res.WriteHeader(http.StatusAccepted)
 	res.Write([]byte(body))
 }
 
@@ -47,7 +50,33 @@ func postReq(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	res.WriteHeader(http.StatusAccepted)
 	res.Write([]byte(body))
+}
+
+type Subj struct {
+	Product string `json:"product"`
+	Price   int    `json:"price"`
+}
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	subj := Subj{"Mikl", 50}
+
+	resp, err := json.Marshal(subj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+func allMethod(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "1")
+	fmt.Fprint(w, "2")
+	w.Write([]byte("3"))
 }
 
 func main() {
@@ -56,6 +85,8 @@ func main() {
 	go mux.HandleFunc(`/api/`, apiRoute)
 	go mux.HandleFunc(`/`, mainPage)
 	go mux.HandleFunc(`/post-test`, postReq)
+	go mux.HandleFunc(`/json`, jsonHandler)
+	go mux.HandleFunc(`/all`, allMethod)
 
 	log.Print("Start server...")
 	if err := http.ListenAndServe(`:8080`, mux); err != nil && err != http.ErrServerClosed {
